bde22c5a41f943ec416bae94f05121a4e116f2b5
Taryana <118675343+Taryana89@users.noreply.github.com>
2023-01-05 07:42:01 +0700

    update Readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+[Hire jobs Demo](https://test-my-t-en-s.vercel.app/)
+
+
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- config/redux/action/profile.js
+++ config/redux/action/profile.js
@@ -1,7 +1,7 @@
 
 export const profileAction = (username = 'nhana557') => async(dispacth) => {
     try {
-        dispacth({ type: "PROFILE_PENDDING", payload: null})
+        dispacth({ type: "PROFILE_PENDING", payload: null})
         const result = await fetch(`https://api.github.com/users/${username}`)
         const data = await result.json();
         dispacth({ type: "PROFILE_SUCCESS", payload: data})
